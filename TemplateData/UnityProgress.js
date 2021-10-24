d6fc3f23ed7c16fe6ea1da736bf7519fe1d59646
Nicsoart <Nicso.art@yandex.com>
2021-10-25 08:39:17 +0900

    index

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -22,5 +22,5 @@ function UnityProgress(unityInstance, progress) {
   if (progress == 1)
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
 
-  LoadingPercentage = progress * 100;
+    sessionStorage.setItem('loading', progress);
 }
